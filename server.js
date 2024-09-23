eef1b4eba3c9e0bba299171807a232a01cb1fcc4
Aryan768 <aryanbhatia9925@gmail.com>
2024-09-23 15:18:12 +0530

    Version

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ function calculateSpeedDurations(durationInSeconds) {
 function formatDuration(totalSeconds) {
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
-  const seconds = totalSeconds % 60;
+  const seconds = (totalSeconds % 60).toFixed(0);
   return `${hours}h ${minutes}m ${seconds}s`;
 }
 
